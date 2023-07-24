4b3b2fb40c73ffd978568e36563dd4b727c2b6b1
Hodaya Rosenberg <hodaya887@gmail.com>
2023-07-24 15:25:07 -0400

    Initial Commit

---
+++ Hebro/HebroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HebroApp.swift
+//  Hebro
+//
+//  Created by Hodaya Rosenberg on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HebroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
