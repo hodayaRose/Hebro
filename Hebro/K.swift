03b107687fa3c8243804d0cff893b7cce4141d1d
Hodaya Rosenberg <hodaya887@gmail.com>
2023-11-17 01:36:57 -0500

    Re-add K.swift to tracking

---
+++ Hebro/K.swift
@@ -0,0 +1,100 @@
+//
+//  Constants.swift
+//  Hebro
+//
+//  Created by Hodaya Rosenberg on 8/10/23.
+//
+
+import Foundation
+import SwiftUI
+//Constants File
+struct K {
+    // App Colors
+    struct Colors {
+        static let bkgColor3 = Color("background3")
+        //brand colors
+        static let pink = Color("card1")
+        
+        
+        static let primery = Color("primarys")
+        static let bkgColor2 = Color("background2")
+        static let bkgColor1 = Color("background1")
+        static let secondary = Color("secondarys")
+        
+        
+      
+    }
+    //SectionData
+    struct Sections {
+        static let title1 = "Bring Hebrew Home"
+        static let title2 = "Color Your Words"
+        static let title3 = "Its Story Time!"
+        static let text1 = "AR Game"
+        static let text2 = "Coloring Book"
+        static let text3 = "story telling in hebrew"
+        
+        
+    }
+    //App Images
+    struct Images {
+        static let Card1 = "Card1"
+        static let Card2 = "Card2"
+        static let Card3 = "Card3"
+        static let Card4 = "Card4"
+        static let Card5 = "Card5"
+        static let eyeglasses = "eyeglasses"
+        static let bag = "bag"
+        static let alef = "alef"
+        static let house = "house"
+        static let hello = "hand.wave"
+        static let dog = "pawprint.fill"
+        static let airplane = "airplane"
+        static let glasses = "eyeglasses"
+       
+        
+        
+        
+        static let logoCircle = "logocircleB"
+        static let Background1 = "Background1"
+        //profile picture
+        static let Avatar = "Avatar"
+        
+       
+    }
+    struct lottieFile {
+        static let success = "success"
+        static let loading = "loading"
+        
+    }
+    
+
+    
+    // Other Constants
+    static let appName = "My SwiftUI App"
+    
+    
+    
+    //payment succeed text
+    static let paymentSuccessMessage = "Thank you for purchasing the Hebrew course! An email confirmation will be sent shortly."
+    static let BottomCardViewText = "text describtion about the course that expends by the texts length"
+    
+    
+  ///Struct  S has to be defined to full personilized experience with this app
+    ///i addition youll need to add a ggogleservice.plist and plist
+//    struct S {
+//        struct contentful {
+//            static let spaceId = {$(contentul spaceId)}
+//            static let accessToken = {$(contentul ACCESSTOKEN)}
+//        }
+//        
+//        // API Endpoints
+//        struct API {
+//            static  let baseURL = "https://api.example.com"
+//            static  let loginEndpoint = "/login"
+//            static  let randomAddress = "https://random-data-api.com/api/address/random_address"
+//            static  let linkedIn = "YOUR_LINKEDIN_URL"
+//        }
+//    }
+    
+}
+
